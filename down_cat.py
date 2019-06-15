eedeef8d6b3935ba927dbad82552cb4d25c9b79b
luolong <lxyzjmll@gmail.com>
2019-06-15 16:14:56 +0800

    发送带图片附件的邮件

--- down_cat.py
+++ down_cat.py
@@ -4,5 +4,5 @@ response = urllib.request.urlopen("https://placekitten.com/500/500")
 
 cat_img = response.read()
 
-with open("cat_300_300.jpg", "wb") as f:
+with open("cat_600_600.jpg", "wb") as f:
     f.write(cat_img)
--- email_send.py
+++ email_send.py
@@ -1,8 +1,34 @@
 import os
 import smtplib
+from email.message import EmailMessage
+import imghdr
 
 EMAIL_ADDRESS = '631581305@qq.com'
 EMAIL_PASSWORD = 'tnigyzqgsvuvbeaf'
+
+msg = EmailMessage()
+msg['Subject'] = '使用emailmessage类发送邮件！！！'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = "631581305@qq.com"
+
+msg.set_content('这是邮件的主要内容')
+
+with open('p5.JPG', 'rb') as f:
+    file_date = f.read()
+    file_type = imghdr.what(f.name)
+    #print(file_type)
+    file_name = f.name
+    #print(file_name)
+
+msg.add_attachment(file_date, maintype='image', subtype=file_type, filename=file_name)
+
+with smtplib.SMTP_SSL("smtp.qq.com", 465) as smtp:
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    smtp.send_message(msg)
+
+
+
+'''
 
 with smtplib.SMTP("smtp.qq.com", 587) as smtp:
     smtp.ehlo()
@@ -16,4 +42,7 @@ with smtplib.SMTP("smtp.qq.com", 587) as smtp:
     body = "《这是邮件内容》"
     msg = f'Subject: {subject}\n\n{body}'
     msg = msg.encode()
-    smtp.sendmail(EMAIL_ADDRESS, '631581305@qq.com', msg)
+    #print(msg)
+    smtp.sendmail(EMAIL_ADDRESS, 'lxyzjmll@gmail.com', msg)
+    
+'''
