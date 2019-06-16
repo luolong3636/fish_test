9a1076ec1590f2bac2cf7b6f27c858d202387e26
luolong <lxyzjmll@gmail.com>
2019-06-16 17:59:30 +0800

    成功接收邮件rar附件

--- email_imap_test.py
+++ email_imap_test.py
@@ -1,86 +1,49 @@
-import email
-import imaplib
-from bs4 import BeautifulSoup
-import os
-import mimetypes
+import imaplib, email, os
 
-username = '631581305@qq.com'
+user = '631581305@qq.com'
 password = 'stnigyzqgsvuvbeaf'
+imap_url = "imap.qq.com"
+attachment_dir = 'c:/fish_test/'
 
-mail = imaplib.IMAP4_SSL("imap.qq.com")
-mail.login(username, password)
+def get_body(msg):
+    if msg.is_multipart():
+        return get_body(msg.get_payload(0))
+    else:
+        return msg.get_payload(None, True)
 
-
-mail.select("inbox")
-#mail.create("Item2")
-
-#mail.list()
-
-result, data = mail.uid('search', None, "ALL")
-
-inbox_item_list = data[0].split()
-
-for item in inbox_item_list:
-    
-    #most_recent = inbox_item_list[-1]
-
-    #oldest = inbox_item_list[0]
-
-    result2, email_data = mail.uid('fetch', item,'(RFC822)')
-
-    raw_email = email_data[0][1].decode("utf-8")
-
-    email_message = email.message_from_string(raw_email)
-
-    to_ = email_message['To']
-    from_ = email_message['From']
-    subject_ = email_message['Subject']
-    date_ = email_message['date']
-    counter = 1
-    for part in email_message.walk():
-        if part.get_content_maintype() == "multipart":
+def get_attachments(msg):
+    for part in msg.walk():
+        if part.get_content_maintype() == 'multipart':
             continue
-        filename = part.get_filename()
-        content_type = part.get_content_type()
-        if not filename:
-            #ext = ".html"
-            ext = mimetypes.guess_extension(content_type)
-            if not ext:
-                ext = '.bin'
-            filename = "msg-part-%08d%s" %(counter, ext)
-        counter += 1
+        if part.get('Content-Disposition') is None:
+            continue
+        fileName = part.get_filename()
 
-    save_path = os.path.join(os.getcwd(), "emails", date_, subject_)
-    
-"""
-    if  not os.path.exists(save_path):
-        os.makedirs(save_path)
-    with open(os.path.join(save_path, filename), 'wb') as fp:
-        fp.write(part.get_payload(decode=True))
-
-
-
-    
-        content_type = part.get_content_type()
-        #print(subject_)
-        #print(content_type)
-        if "plain" in content_type:
-            #print(part.get_payload())
-            pass
-        elif "html" in content_type:
-            html_ = part.get_payload()
-            soup = BeautifulSoup(html_, "html.parser")
-            text = soup.get_text()
-            print(subject_)
-            print(text)
-        else:
-            pass
-            #print(content_type)
-        
-"""       
+        if bool(fileName):
+            filePath = os.path.join(attachment_dir, fileName)
+            with open(filePath, 'wb') as f:
+                f.write(part.get_payload(decode=True))
             
 
-        
+
+def search(key,value,con):
+    result,data = con.search(None,key,'"{}"'.format(value))
+    return data
+
+def get_emails(result_bytes):
+    msgs = []
+    for num in result_bytes[0].split():
+        typ, data =con.fetch(num, '(RFC822)')
+        msgs.append(data)
+    return msgs
 
 
-    #email_message.get_payload()
+con = imaplib.IMAP4_SSL(imap_url)
+con.login(user, password)
+
+con.select('INBOX')
+
+result, data = con.fetch(b'34', 'RFC822')
+raw = email.message_from_bytes(data[0][1])
+#print(get_body(raw))
+get_attachments(raw)
